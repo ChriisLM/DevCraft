dfd6b5597feb9859c0f46605183e0b618a61f572
ChriisLM <christianlezcano878@gmail.com>
2025-03-24 21:46:11 -0500

    style: Change styles and add dark mode

--- src/components/sections/Footer.tsx
+++ src/components/sections/Footer.tsx
@@ -1,25 +1,26 @@
-import { Github } from "../../assets/Icons";
+import { Github } from "../../utils/Icons";
 
 export function Footer() {
   return (
-    <footer className="bg-gray-900 text-white h-auto w-auto py-6 mt-10">
-      <div className="container mx-auto flex items-center justify-center space-x-10">
-        <div className="text-sm flex items-center space-x-2">
+    <footer className="bg-light-secondary/80 dark:bg-dark-secondary/70 text-white h-auto w-auto py-6">
+      <div className="container mx-auto flex items-center justify-around space-x-20">
+        <div className="text-color-light-secondary dark:text-color-dark-secondary text-sm flex items-center space-x-2">
           <p className="">
             &copy; {new Date().getFullYear()} Hecho con ❤️ por
           </p>
           <p>
-            <strong className=" text-yellow-300"> @ChriisLM</strong>
+            <strong className="text-blue-700 dark:text-yellow-300"> @ChriisLM</strong>
           </p>
         </div>
-        <div className="flex">
+        <div className="flex text-color-light-secondary dark:text-color-dark-secondary">
           <a
             href="https://github.com/ChriisLM/DevCraft"
             target="_blank"
             rel="noopener noreferrer"
-            className="hover:text-gray-400 transition"
+            className="flex space-x-2 items-center"
           >
-            <Github className="size-4" />
+            <Github className="size-5" />
+            <span className="text-sm hover:text-blue-500 transition-colors duration-300">Github</span>
           </a>
         </div>
       </div>
