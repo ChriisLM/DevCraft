6ec290879049d0ee7743a8b4c9b27d674672ce8d
ChriisLM <christianlezcano878@gmail.com>
2025-03-24 21:48:05 -0500

    feat: Create title component for home

---
+++ src/components/ui/TitleHome.tsx
@@ -0,0 +1,11 @@
+export function TitleHome() {
+  return (
+    <div className="flex justify-center items-center space-x-4 mt-16 z-10">
+      <span className="h-[2px] w-96 border dark:border-color-dark-secondary"></span>
+      <h2 className="font-semibold text-4xl text-color-light-secondary dark:text-color-dark-secondary ">
+        Herramientas
+      </h2>
+      <span className="h-[2px] w-96 border dark:border-color-dark-secondary"></span>
+    </div>
+  );
+}
