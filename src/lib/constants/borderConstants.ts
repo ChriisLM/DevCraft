8b3a07a64cde8475d7f92df8a693c9321d78fe42
ChriisLM <christianlezcano878@gmail.com>
2025-04-14 21:31:19 -0500

    feat: Create constants of border

---
+++ src/lib/constants/borderConstants.ts
@@ -0,0 +1,13 @@
+import { BorderRadius } from "../types/border";
+
+export const DEFAULT_BORDER_WIDTH = 2;
+export const DEFAULT_BORDER_STYLE = "solid";
+export const DEFAULT_BORDER_COLOR = "#3b82f6";
+export const DEFAULT_BACKGROUND_COLOR = "#ffffff";
+
+export const DEFAULT_BORDER_RADIUS: BorderRadius = {
+  topLeft: 8,
+  topRight: 8,
+  bottomRight: 8,
+  bottomLeft: 8,
+};
